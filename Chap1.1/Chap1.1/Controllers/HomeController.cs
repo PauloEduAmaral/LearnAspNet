a3fa84ccb76626284e8c4427dd25e0feb092fe92
PauloEduAmaral <paulo.e.amaral@gmail.com>
2020-01-09 19:16:26 -0300

    Created view MyView and added a call to it in HomeController

--- Chap1.1/Chap1.1/Controllers/HomeController.cs
+++ Chap1.1/Chap1.1/Controllers/HomeController.cs
@@ -1,43 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Chap1._1.Models;
 
 namespace Chap1._1.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public ViewResult Index()
         {
-            return View();
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Goodd morning" : "Good Afternoon";
+            return View("MyView");
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
