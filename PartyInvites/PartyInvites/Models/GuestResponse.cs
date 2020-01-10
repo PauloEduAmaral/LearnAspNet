6211e9b11fc099532852c65cc11d0def0c8a755a
Paulo Amaral <paulo.e.amaral@gmail.com>
2020-01-10 15:01:23 -0300

    Included validation in RsvpForm.

--- PartyInvites/PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/PartyInvites/Models/GuestResponse.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace PartyInvites.Models
 {
     public class GuestResponse
     {
+        [Required(ErrorMessage ="Enter your name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage ="Enter your email address")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage ="Enter a valid email address")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage ="Enter your phone number")]
         public string Phone { get; set; }
+
+        [Required(ErrorMessage ="Specify whether you'll attend")]
         public bool? WillAttend { get; set; }
     }
 }
