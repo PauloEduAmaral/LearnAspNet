6d991c01a46c286a66cbb23b7721f12aeac32028
PauloEduAmaral <paulo.e.amaral@gmail.com>
2020-03-08 12:00:25 -0300

    Criadas classes POCO.

---
+++ AplicacaoComCodeFirst/AplicacaoComCodeFirst/Models/BlogContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace AplicacaoComCodeFirst.Models
+{
+    public class BlogContext : DbContext
+    {
+        public DbSet<Posts> Posts { get; set; }
+        public DbSet<Categorias> Categorias { get; set; }
+    }
+}
