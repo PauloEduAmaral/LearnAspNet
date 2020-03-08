22875d3e00d5b82dac8528db1e1fe889b62a32bc
PauloEduAmaral <paulo.e.amaral@gmail.com>
2020-03-08 14:10:56 -0300

    Adicionados modelos de metadado.

---
+++ CadeMeuMedico/CadeMeuMedico/Models/EspecialidadeMetadado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CadeMeuMedico.Models
+{
+    [MetadataType(typeof(EspecialidadeMetadado))]
+    public partial class Especialidades
+    {
+
+    }
+
+    public class EspecialidadeMetadado
+    {
+
+    }
+}
